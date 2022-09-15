189b939b98e0c58e636e268592aa17b3021fdf26
Ivgeniay <45053991+Ivgeniay@users.noreply.github.com>
2022-09-15 10:13:39 +0300

    15.09.22

--- MangoRestaurant/Mando.Services.ProfuctAPI/DbContexts/ApplicationDbContext.cs
+++ MangoRestaurant/Mando.Services.ProfuctAPI/DbContexts/ApplicationDbContext.cs
@@ -12,5 +12,50 @@ namespace Mando.Services.ProductAPI.DbContexts
         }
 
         public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 1,
+                    Name = "Samosa",
+                    Price = 15,
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut bibendum mauris. Donec vel nulla ut lacus pulvinar volutpat malesuada eu velit. Vestibulum nibh mauris, sagittis eu mattis sit amet, consequat et felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eu mauris eu sapien maximus pretium at id leo. Nullam augue metus, ullamcorper ut sodales vitae, ultrices et dolor. Sed sagittis leo eu nibh rhoncus maximus. Donec enim odio, tristique vel turpis nec, vulputate scelerisque neque.",
+                    ImageUrl = "",
+                    CategoryName = "Snack"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 2,
+                    Name = "Kebab",
+                    Price = 13.99,
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut bibendum mauris. Donec vel nulla ut lacus pulvinar volutpat malesuada eu velit. Vestibulum nibh mauris, sagittis eu mattis sit amet, consequat et felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eu mauris eu sapien maximus pretium at id leo. Nullam augue metus, ullamcorper ut sodales vitae, ultrices et dolor. Sed sagittis leo eu nibh rhoncus maximus. Donec enim odio, tristique vel turpis nec, vulputate scelerisque neque.",
+                    ImageUrl = "",
+                    CategoryName = "Snack"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 3,
+                    Name = "Sweet Pie",
+                    Price = 10.99,
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut bibendum mauris. Donec vel nulla ut lacus pulvinar volutpat malesuada eu velit. Vestibulum nibh mauris, sagittis eu mattis sit amet, consequat et felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eu mauris eu sapien maximus pretium at id leo. Nullam augue metus, ullamcorper ut sodales vitae, ultrices et dolor. Sed sagittis leo eu nibh rhoncus maximus. Donec enim odio, tristique vel turpis nec, vulputate scelerisque neque.",
+                    ImageUrl = "",
+                    CategoryName = "Dessert"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 4,
+                    Name = "Vegetable curry",
+                    Price = 13,
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut bibendum mauris. Donec vel nulla ut lacus pulvinar volutpat malesuada eu velit. Vestibulum nibh mauris, sagittis eu mattis sit amet, consequat et felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eu mauris eu sapien maximus pretium at id leo. Nullam augue metus, ullamcorper ut sodales vitae, ultrices et dolor. Sed sagittis leo eu nibh rhoncus maximus. Donec enim odio, tristique vel turpis nec, vulputate scelerisque neque.",
+                    ImageUrl = "",
+                    CategoryName = "Entree"
+                });
+        }
     }
 }
