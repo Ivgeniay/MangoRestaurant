70d907e9cd2dd10e5a0f5c79f3d0dcc572cca62f
Ivgeniay <45053991+Ivgeniay@users.noreply.github.com>
2022-09-19 14:19:41 +0300

    19.09.2022

--- MangoRestaurant/Mando.Services.ProfuctAPI/DbContexts/ApplicationDbContext.cs
+++ MangoRestaurant/Mando.Services.ProfuctAPI/DbContexts/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace Mando.Services.ProductAPI.DbContexts
         }
 
         public DbSet<Product> Products { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
